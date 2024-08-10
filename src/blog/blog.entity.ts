102132503ef052b296a5aed2a56673e6e342a587
devellybutton <127278410+devellybutton@users.noreply.github.com>
2024-08-10 20:30:10 +0900

    프로젝트 구조 세팅

---
+++ src/blog/blog.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Blog {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column({ default: true })
+  published: boolean;
+}
